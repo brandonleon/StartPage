a7bf63f933e7236378ce6aa5b73ef53923a61ad2
Brandon Leon <brandonleon@pm.me>
2022-10-07 19:27:06 -0700

    Function to update count of tags.

--- services/tags.py
+++ services/tags.py
@@ -26,3 +26,23 @@ class Tag:
                     "INSERT INTO tags (id, name, count) VALUES (:id, :name, 1)",
                     {"id": self.id, "name": self.name},
                 )
+
+    # Update the tag count
+    def update_count(self) -> int:
+        con = sqlite3.connect(db_path)
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        cur.execute(
+            "SELECT count(*) as count FROM tag_link_map WHERE tag_id = :id LIMIT 1",
+            {"id": self.id},
+        )
+
+        count = cur.fetchone()["count"]
+        # update the count in the tags table
+        with con as cur:
+            cur.execute(
+                "UPDATE tags SET count = :count WHERE id = :id",
+                {"id": self.id, "count": count},
+            )
+
+        return count
